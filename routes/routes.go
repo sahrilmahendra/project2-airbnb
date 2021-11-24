d07ef51c40ea88ec74ee98e3a93080b2a14a68e6
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-11-24 23:15:47 +0700

    menambahkan fitur users

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"project2/constants"
+	"project2/controllers"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -11,12 +12,18 @@ func New() *echo.Echo {
 
 	e := echo.New()
 	// users
+	e.POST("/users", controllers.CreateUserControllers)
+	e.POST("/login", controllers.LoginUserControllers)
 
 	// group JWT
 	j := e.Group("/jwt")
 	j.Use(middleware.JWT([]byte(constants.SECRET_JWT)))
 
 	// users
+	j.GET("/users/:id", controllers.GetUserControllers)
+	j.PUT("/users/:id", controllers.UpdateUserControllers)
+	j.DELETE("/users/:id", controllers.DeleteUserControllers)
+
 	return e
 
 }
